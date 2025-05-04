89052435558a1566d00b83a0c4f90f1d7b62c289
Samuel-John-Wilson <Samuel.John.Wilson@outlook.com>
2025-05-04 21:17:20 +1000

    0.0.2: black screen, player rotation added

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 # this allows us to use code from the Open Source pygame library
 import pygame
 
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT, ASTEROID_MIN_RADIUS, ASTEROID_MAX_RADIUS, ASTEROID_SPAWN_RATE, ASTEROID_KINDS
+from constants import *
+from player import *
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+clock = pygame.time.Clock()
+
 def main():
-    pygame.init
+    pygame.init()
+    
+    dt = 0
     
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    pc = player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
     #Game loop
     while True:
@@ -19,7 +25,16 @@ def main():
                 return
           
           screen.fill(000, rect=None, special_flags=0)
+          
+          
+          pc.update(dt)
+          pc.draw(screen)
+          
+          
+          
+          
           pygame.display.flip()
+          dt = clock.tick(60)/1000
 
 
 
